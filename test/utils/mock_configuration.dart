301ece52c11d4753a0a621698862f5c8fb224960
hany <hanysayed.a@gmail.com>
2022-06-09 17:51:41 +0200

    copy build.yaml to exmple

--- test/utils/mock_configuration.dart
+++ test/utils/mock_configuration.dart
@@ -48,6 +48,6 @@ void mockConfigDefaultValues(WidgetConfiguration config) {
   when(config.marketingBrandName).thenReturn('Brand Name');
   when(config.affiliatesUrl).thenReturn('');
   when(config.privacyUrl).thenReturn('');
-  when(config.findClinicButtonInChatEnabled).thenReturn(false);
+  when(config.findClinicButtonInChatEnabled).thenReturn(true);
 
 }
